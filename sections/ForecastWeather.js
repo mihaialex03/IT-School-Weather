b12cfbc1d333fb0f29d5701840949337c8c1ad13
mihaialex03 <alexandrumihai.03@icloud.com>
2024-03-28 18:03:07 +0200

    scroll changes

--- sections/ForecastWeather.js
+++ sections/ForecastWeather.js
@@ -44,7 +44,7 @@ function displayWeatherForecast(city) {
             const weatherIcon = getWeatherIcon(weather[0].icon);
             // inseram in html
             weatherForecastContainer.innerHTML += `
-                <div class="weather-forecast-box d-flex justify-content-between align-items-center w-100 border rounded mb-3">
+                <div class="weather-forecast-box d-flex justify-content-between align-items-center w-100 border rounded mb-3 p-3">
                     <div>${hour}</div>
                     <div><img src="${weatherIcon}"/></div>
                     <div class="fs-3"><strong>${temperature}°C</strong></div>
